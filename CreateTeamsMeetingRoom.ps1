3409459791b5e92ba4734588af1566e0d70ce76e
Christian Frohn <50671696+vFrohn@users.noreply.github.com>
2021-08-19 12:56:26 +0200

    Update CreateTeamsMeetingRoom.ps1

--- CreateTeamsMeetingRoom.ps1
+++ CreateTeamsMeetingRoom.ps1
@@ -11,18 +11,21 @@ Connect-MsolService
 #Used in Teams
 $PhoneNumber = "" #The number to be used in the conference room (remember country code fx. +45)
 $TeamsIpPhonePolicy = "" #The policy to be used in the conference room (Get-CsTeamsIPPhonePolicy)
-
+$CallingPolicy = "" #The calling policy to be used by the conference phone user
 
 #Used in Exchange / Msol
 $RoomMail = "" #RoomName@yourdomain.it
 $RoomName = "" #The rooms name
-$RoomPassword = "The password for the room mailbox"
-$TeamsRoomLicense = "YourCOMPANY:MEETING_ROOM" #If in doubt, run Get-MsolAccountSku and look for "MEETING_ROOM"
+$RoomPassword = ""
+$TeamsRoomLicense = "YOURCOMPANYNAME:MEETING_ROOM" #If in doubt, run Get-MsolAccountSku and look for "MEETING_ROOM"
 $UsageLocation = "" #US or DK fx.
 
 ##########################################################################################################################################################################################################
 #Createting the room mailbox
 New-Mailbox -MicrosoftOnlineServicesID $RoomMail -Name $RoomName -Room -RoomMailboxPassword (ConvertTo-SecureString -String $RoomPassword  -AsPlainText -Force) -EnableRoomMailboxAccount $true -Verbose
+
+#Wait for mailbox for be completed.
+Start-Sleep 20
 
 #Set password to never expire and setting the usage location of the room mailbox
 Set-MsolUser -UserPrincipalName $RoomMail -PasswordNeverExpires $true -UsageLocation $UsageLocation -Verbose
@@ -49,24 +52,24 @@ Set-MailboxFolderPermission -Identity $RoomMail -User default -AccessRights Publ
 ##########################################################################################################################################################################################################
 
 #Need to wait for the cloud to find the new room mailbox
-Start-Sleep 5000
+Start-Sleep 500
 
 ##########################################################################################################################################################################################################
 #
 #Teams configuration
+#Get reg. pool
+$GetRegPool = Get-CsOnlineUser $RoomMail | Select-Object RegistrarPool
+
+#Set LineURI #Enable EnterpriseVoice
+Set-CsUser $RoomMail -EnterpriseVoiceEnabled $true 
+Set-CsUser $RoomMail -HostedVoiceMail $true
+Set-CsUser $RoomMail -OnPremLineURI tel:$PhoneNumber
 
 #Enable the room mailbox to function as a teams device
-Enable-CsMeetingRoom -Identity $RoomMail -SipAddressType “EmailAddress” -RegistrarPool “sippoolAM30E26.infra.lync.com“ -Verbose 
-
-#Enable EnterpriseVoice
-Set-CsMeetingRoom -Identity $RoomMail -EnterpriseVoiceEnabled $true -Verbose 
-
-#Enable VoiceMail
-Set-CsMeetingRoom -Identity $RoomMail -HostedVoiceMail $true -Verbose 
-
-#Set phone number
-Set-CsMeetingRoom -Identity $RoomMail -LineURI "tel:$PhoneNumber" -Verbose
+Enable-CsMeetingRoom -Identity $RoomMail -RegistrarPool $GetRegPool.RegistrarPool -SipAddressType “EmailAddress” -Verbose 
 
 #Set the Teams device policy 
 Grant-CsTeamsIPPhonePolicy -Identity $RoomMail -PolicyName $TeamsIpPhonePolicy -Verbose
+
+Grant-CsTeamsCallingPolicy -Identity $RoomMail -PolicyName $CallingPolicy -Verbose
 
